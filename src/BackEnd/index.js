e67359927cc78476626dabc80d8fc459f7e89e2f
avi <avatansharma13@gmail.com>
2025-03-17 19:00:03 +0530

    Deploy to gh-pages

--- src/BackEnd/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { ButtonArray } = require("../FrontEnd/pages/Home");
-
-const app = express();
-const router =express.Router();
-
-router.use("/calculaton" ,ButtonArray)
-
-module.exports = router;  
